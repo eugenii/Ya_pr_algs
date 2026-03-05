002b1b17b5608ebe8a70115f4bf75548c3425ef0
eugenii <eugenii.cher@gmail.com>
2026-03-05 10:47:03 +0300

    take 2

--- sprint_03/final/B_effective_quick_sort.py
+++ sprint_03/final/B_effective_quick_sort.py
@@ -1,31 +1,52 @@
 # B Эффективная быстрая сортировка.
 
 
-def comparator(a:tuple[str, int, int], b:tuple[str, int, int]) -> bool:
+def compare(a:tuple[int, int, str], b:tuple[int, int, str]) -> bool:
     """
     Возвращает True, если a < b.
     """
-    pass
+    return a < b
 
-def partition(array:list[tuple[str, int, int]], pivot:tuple[str, int, int]):
+
+def quick_sort(array, start, end):
     """
-    Возвращает массив, разбитый на три части:
-    - левая часть содержит все значения, меньшие pivot
-    - средняя часть содержит все значения, равные pivot
-    - правая часть содержит все значения, большие pivot
+    Quick sort.
     """
-    pass
+    if start >= end:
+        return
 
-def quicksort(array:list[tuple[str, int, int]]):
-    pass
+    # Выбираем пивот (например, посередине)
+    pivot = array[(start + end) // 2]
+    left, right = start, end
+
+    while left <= right:
+        # Ищем элемент слева, который должен быть справа
+        while compare(array[left], pivot): # array[left] < pivot
+            left += 1
+        # Ищем элемент справа, который должен быть слева
+        while compare(pivot, array[right]): # array[right] > pivot
+            right -= 1
+        
+        if left <= right:
+            array[left], array[right] = array[right], array[left]
+            left += 1
+            right -= 1
+
+    # Рекурсивно запускаем для левой и правой части
+    quick_sort(array, start, right)
+    quick_sort(array, left, end)
 
 
-n = int(input())
-arr = []
-for i in range(n):
-    name, score, penalti = input().split()
-    arr.append((name, int(score), int(penalti)))
-print(*arr, sep='\n')
+def main():
+    n = int(input())
+    arr = []
+    for i in range(n):
+        name, tasks, penalti = input().split()
+        arr.append((-int(tasks), int(penalti), name))
+    quick_sort(arr, 0, len(arr)-1)
+    for member in arr:
+        print(member[2])
 
-quicksort(arr)
-print(*arr, sep='\n')
+
+if __name__ == '__main__':
+    main()
