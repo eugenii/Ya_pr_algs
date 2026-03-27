f45ecb1e3c666f358838ac7f40626db93e8191af
eugenii <eugenii.cher@gmail.com>
2026-03-27 14:19:36 +0300

    I_different_BST

---
+++ sprint_05/I_different_BST.py
@@ -0,0 +1,13 @@
+# Различные деревья поиска.
+
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[0] = 1
+dp[1] = 1
+
+for k in range(2, n + 1):
+    for i in range(1, k + 1):
+        dp[k] += dp[i - 1] * dp[k - i]
+
+print(dp[n])
